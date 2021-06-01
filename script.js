8eb1ace0046ba5af2ed2670023247c9db768aacc
brandontor <53887678+brandontor@users.noreply.github.com>
2021-06-01 15:12:13 -0400

    added onload animations on first page and profile picture

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-const aboutMeBTN = document.getElementById('aboutMeBTN')
-const interestContainer = document.querySelector('.interestcontainer')
-
-console.log(interestContainer)
+const aboutMeBTN = document.getElementById('aboutMeBTN');
+const interestContainer = document.querySelector('.interestcontainer');
+const socials = document.querySelectorAll('.socialIcon');
+const toggle = document.querySelector('.toggle');
+const mainPic = document.querySelector('.mainPic');
+const enterBTN = document.querySelector('.enterBTN');
+const body = document.body;
 //Object selector 
 
 
-aboutMeBTN.addEventListener('click', showMeInterests )
-//Event Listener
+body.onload = function() {
+    mainPic.style.opacity = 1;
+    enterBTN.style.opacity = 1;
+}
+
+aboutMeBTN.addEventListener('click', function (e) {
+        showMeInterests(e);
+        showMyMenu();
+})
+//Event Listener^
 
 
 function showMeInterests(e) {
     aboutMeBTN.style.opacity = 0;
     interestContainer.style.opacity = 100;
-
     e.preventDefault();
 }
 //Change opacity function 
 
-//To-do
-//Function to set display on btn 0
-//set timeout function to change opacity on picture container
-//change styling on interest container to have the hover state more noticeable
+function showMyMenu() {
+    socials.forEach(function(target) {
+        target.style.opacity = 1;
+    });
+    toggle.style.opacity = 1;
+}
+
